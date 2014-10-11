6d9918635d07691f2d0446e1fca88157a9c01e58
Chris Dunkers <cmdunkers@gmail.com>
2014-10-10 21:06:00 -0400

    initial outline of the code

---
+++ src/oddbot_boot_brain.cpp
@@ -0,0 +1,20 @@
+# include "oddbot_boot/oddbot_boot_brain.h"
+
+oddbot_boot_brain::oddbot_boot_brain(){
+	//subscribe to oddbot_boot
+	//publish to oddbot_boot_stop
+}
+
+void oddbot_boot_brain::get_info(oddbot_boot::OddbotBoot constPtr &boot_msg){
+	
+	//get info
+	
+	//publish to oddbot_boot_stop
+	
+}
+
+int main(int argc, char** argv){
+	
+	//how to stop the node??
+	
+}
---
+++ src/oddbot_boot_module.cpp
@@ -0,0 +1,22 @@
+oddbot_boot_module::oddbot_boot_module(){
+	//subscribe to oddbot_boot
+	//publish to oddbot_boot_stop
+}
+
+void oddbot_boot_module::get_info(oddbot_boot::OddbotBootStop constPtr &stop_msg){
+	
+	//check my IP address
+	
+	//set stop running flag
+	
+}
+
+void oddbot_boot_module::send_info(){
+	
+}
+
+int main(int argc, char** argv){
+	
+	//stop when stop running falg is true
+	
+}
